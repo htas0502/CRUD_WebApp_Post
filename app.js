c3a30e33a4ed92a1de747b50ce6aa1a5670b4828
htas0502 <Hoangtuanhsang.5555@gmail.com>
2023-07-31 19:14:02 +0700

    Tao giao dien Nav, Home, About, Add Post

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 
 // Khai bao express de co the xai duoc express
 const express = require('express')
+// *** Khai bao Handlebars
+const { engine } = require('express-handlebars')
 // Khai bao database 
 const connectDB = require('./config/db')
+
 
 // ** Import Routes
 const posts = require('./routes/posts')
 
 // Khoi dong app
 const app = express()
+// *** Khoi dong Handlebars Middleware
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
 // Khoi dong express middleware ( cai nay la thu tuc can lam truoc khi tao 1 app )
 app.use(express.json())
 
 // * Ket noi CSDL
 connectDB()
+
+// *** Mot so routes co ban, co the dua vao file rieng trong thu muc routes:
+app.get('/', (req, res) => res.render('index'))
+app.get('/about', (req, res) => res.render('about'))
 
 // ** Su dung Routes
 app.use('/posts', posts)        // Moi duong dan ma huong toi '/post' nay thi no se chuyen ve file posts.js
--- routes/posts.js
+++ routes/posts.js
@@ -8,9 +8,15 @@ const router = express.Router()
 const Post = require('../models/Post')
 
 // Test Router:
-router.get('/', (req, res) => {
-    // res.send tra ve 1 thong diep
-    res.send('Day la Router Post!')
+// router.get('/', (req, res) => {
+//     // res.send tra ve 1 thong diep
+//     res.send('Day la Router Post!')
+// })
+
+
+// Hien thi for tao post:
+router.get('/add', (req, res) => {
+    res.render('posts/add')
 })
 
 module.exports = router
